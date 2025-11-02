ee73452ad8738725dff82034fa6ed5522453d581
Takumi Mitsuhashi <takumimitsu22@gmail.com>
2025-11-02 20:34:28 +0900

    completed

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pic_192.png',
+            src: '/StartIcon_192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/pic_512.png',
+            src: '/StartIcon_512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/pic_512.png',
+            src: '/StartIcon_512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
